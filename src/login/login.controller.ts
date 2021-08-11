17992f01bba2220b0b7be06d4335300acc9b936d
Gustavo Gimenez <gustavogimenezs@icloud.com>
2021-08-10 23:06:29 -0300

    Creating validate class to login

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { LoginDto } from './login.dto';
 import { LoginService } from './login.service';
 
 @Controller('login')
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
   @Post()
-  public makeLogin(@Body() body): any {
-    return this.loginService.makeUserLogin(body.email, body.password);
+  public makeLogin(@Body(ValidationPipe) loginDto: LoginDto): any {
+    return this.loginService.makeUserLogin(loginDto.email, loginDto.password);
   }
 }
