ab8b9b8326803dd828bc2b5efd7aeef349f6a53f
Gustavo Gimenez <gustavogimenezs@icloud.com>
2021-08-13 20:58:16 -0300

    Fixing catlog imports

--- src/catalog/catalog.controller.spec.ts
+++ src/catalog/catalog.controller.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProductsController } from './catalog.controller';
+import { CatalogController } from './catalog.controller';
 
 describe('CatalogController', () => {
-  let controller: ProductsController;
+  let controller: CatalogController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProductsController],
+      controllers: [CatalogController],
     }).compile();
 
-    controller = module.get<ProductsController>(ProductsController);
+    controller = module.get<CatalogController>(CatalogController);
   });
 
   it('should be defined', () => {
